a0c28c4f2198a5145d3df3df80c7c04043261a75
Austin Asbury <acasbury25@aol.com>
2019-10-27 14:41:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,5 +11,14 @@ module FormsLab
     get '/new' do
       erb :'pirates/new'
     end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirates][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+      erb :'pirates/show'
+    end
   end
 end
